eba04cce1f1a29f7c76d01854863bbcfa68bfd21
ポキ <aman_a@aol.com>
2020-10-02 00:46:12 +0500

    Added Custom Client
    - a class for Client
    - Logger ofcourse

--- thebot/__init__.py
+++ thebot/__init__.py
@@ -16,4 +16,26 @@ logging.basicConfig(
 )
 LOGS = logging.getLogger(__name__)
 
-dankbot = Client('dank', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+name = 'dank'
+
+class dankbot(Client):
+    def __init__(self, name):
+        """Custom Pyrogram Client."""
+        super().__init__(
+            name,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=TOKEN
+        )
+
+
+    async def start(self):
+        await super().start()
+        print("Bot started. Hi.")
+
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot stopped. Bye.")
+
+dankbot = dankbot(name)
