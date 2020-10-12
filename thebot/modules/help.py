8c36649db7bea886718c491720125c1e4f44c9fd
Chizuru <chizuru@kanojo.tk>
2020-10-12 08:12:15 +0530

    update help

--- thebot/modules/help.py
+++ thebot/modules/help.py
@@ -3,4 +3,4 @@ from pyrogram import filters
 from thebot import dankbot
 
 @dankbot.on_message(filters.command('help'))
-def help(_, m): m.reply_text('Available cmds for now :\n `/anime` - search anime on AniList\n `/manga` - search manga on Anilist\n `/character` - search character on Anilist\n `/airing` - check airing status of an anime.')
+def help(_, m): m.reply_text('Available cmds for now :\n `/anime` - search anime on AniList\n `/manga` - search manga on Anilist\n `/character` - search character on Anilist\n `/airing` - check airing status of an anime\n `/wa` - find what anime a media is from.')
