55780f99c928b5536faea35b7e3703cb69a67321
Chizuru <chizuru@kanojo.tk>
2020-10-12 08:04:23 +0530

    [EXPERIMENTAL] add whatanime
    Co-Authored-By: blank X <theblankx@protonmail.com>

--- thebot/__main__.py
+++ thebot/__main__.py
@@ -6,7 +6,7 @@ import traceback
 import logging
 from pyrogram import  idle, Client
 from thebot import dankbot
-from thebot.modules import start, help, dev, inline, anilist
+from thebot.modules import start, help, dev, inline, whatanime, anilist
 
 dankbot.start()
 idle()
