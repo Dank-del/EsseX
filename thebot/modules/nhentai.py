b77ae2834e9bff89bf6952206596f79f5f412830
Wackyator <pratapsinghtarun9@gmail.com>
2020-10-14 16:39:44 +0530

    fixed nhentai

--- thebot/modules/nhentai.py
+++ thebot/modules/nhentai.py
@@ -12,7 +12,7 @@ from thebot import dankbot, telegraph
 @dankbot.on_message(filters.command('nhentai'))
 async def nhentai(client, message):
     query = message.text.split(" ")[1]
-    title, tags, artist, total_pages, post_url = nhentai_data(query)
+    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(query)
     await message.reply_text(
          f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
         reply_markup=InlineKeyboardMarkup(
