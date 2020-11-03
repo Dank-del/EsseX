5382807b1078893554419678ce095bc534030fdc
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-11-03 10:16:35 +0530

    Added Inline Manga Search

--- thebot/modules/inline.py
+++ thebot/modules/inline.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pyrogram import errors, __version__
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent, InlineQueryResultArticle, InlineQueryResultPhoto
 from pyrogram.errors import PeerIdInvalid
-from thebot.modules.anilist import url, anime_query, shorten
+from thebot.modules.anilist import url, anime_query, manga_query, shorten
 from thebot.modules.nhentai import nhentai, nhentai_data
 
 import aiohttp
@@ -119,4 +119,49 @@ async def inline_query_handler(client, query):
                                         results=answers,
                                         cache_time=0
                                         )
-
+    elif string.split()[0] == "manga":
+        if len(string.split()) == 1:
+            await client.answer_inline_query(query.id,
+                                            results=answers,
+                                            switch_pm_text="Search Manga",
+                                            switch_pm_parameter="start"
+                                            )
+            return
+        search = string.split(None, 1)[1]
+        variables = {'search' : search}
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json={'query': manga_query, 'variables': variables}) as resp:
+                r = await resp.json()
+                json = r['data'].get('Media', None)
+                if json:
+                    msg = f"**{json['title']['romaji']}** (`{json['title']['native']}`)\n**Status**: {json['status']}\n**Year**: {json['startDate']['year']}\n**Score**: {json['averageScore']}\n**Genres**: `"
+                    for x in json['genres']: 
+                        msg += f"{x}, "
+                    msg = msg[:-2] + '`\n'
+                    description = json.get('description', 'N/A').replace('<i>', '').replace('</i>', '').replace('<br>', '')
+                    info = json.get('siteUrl')
+                    if info:
+                        buttons = InlineKeyboardMarkup([[InlineKeyboardButton("More Info", url=info)]])
+                    else:
+                        buttons = None
+                    msg += shorten(description, info)
+                    banner_url = json.get('bannerImage')
+                    if banner_url:
+                        answers.append(InlineQueryResultPhoto(
+                            caption=msg,
+                            photo_url=banner_url,
+                            parse_mode="markdown",
+                            title=f"{json['title']['romaji']}",
+                            description=f"{json['startDate']['year']}",
+                            reply_markup=buttons))
+                    else:
+                        answers.append(InlineQueryResultArticle(
+                            title=f"{json['title']['romaji']}",
+                            description=f"{json['averageScore']}",
+                            input_message_content=InputTextMessageContent(msg, parse_mode="markdown", disable_web_page_preview=True),
+                            reply_markup=buttons))
+        await client.answer_inline_query(query.id,
+                                        results=answers,
+                                        cache_time=0,
+                                        is_gallery=False
+                                        )
