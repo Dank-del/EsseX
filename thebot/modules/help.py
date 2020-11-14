6679c93c42394ab4b20606ec9cf3d8eeba02a3aa
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-11-14 21:43:08 +0530

    Added Inline Character + Airing Search

--- thebot/modules/help.py
+++ thebot/modules/help.py
@@ -1,6 +1,6 @@
 import asyncio
-from pyrogram import filters
 from thebot import dankbot
+from pyrogram import filters
 from .callback import callback_data
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -9,8 +9,13 @@ buttons = [
     InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
     InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
     InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
+    ],
+    [
+        InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
+        InlineKeyboardButton('Character', switch_inline_query_current_chat='char '),
     ]
 ]
+
 @dankbot.on_callback_query(callback_data(['help']))
 async def chelp(_, c):
     await c.edit_message_text('Available cmds for now :\n /animeinfo - search anime on AniList\n /mangainfo - search manga on Anilist\n /charinfo - search character on Anilist\n /airinfo - check airing status of an anime\n /wa by replying to a media - find what anime a media is from\n /nhentai ID - returns the nhentai in telegraph instant preview format.', reply_markup=InlineKeyboardMarkup(buttons))
