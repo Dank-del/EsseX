e810b81fc7d8844ec11384935689f18bdd94765e
Chizuru <chizuru@kanojo.tk>
2021-01-11 00:35:13 +0530

    fix /help in pm
    im dumb lmao

--- thebot/modules/help.py
+++ thebot/modules/help.py
@@ -29,6 +29,17 @@ async def chelp(_, client):
 @dankbot.on_message(~filters.me & filters.command('help', prefixes='/'), group=8)
 @capture_err
 async def help(_, message):
+    buttons = [
+        [
+            InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
+            InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
+            InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
+        ],
+        [
+            InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
+            InlineKeyboardButton('Character', switch_inline_query_current_chat='char '),
+        ]
+    ]
     if message.chat.type == "private":
        await message.reply_text(
         'Available cmds for now :\n /animeinfo - search anime on AniList\n /mangainfo - search manga on Anilist\n /charinfo - search character on Anilist\n /airinfo - check airing status of an anime\n /wa by replying to a media - find what anime a media is from\n /nhentai ID - returns the nhentai in telegraph instant preview format.',
